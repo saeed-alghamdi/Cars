f466b3b8548647807561d11a821715b78da77f6d
saeed-alghamdi <121924411+saeed-alghamdi@users.noreply.github.com>
2026-05-23 12:19:10 +0300

    .

--- Cars/DataPersistanceController.swift
+++ Cars/DataPersistanceController.swift
@@ -9,21 +9,6 @@ import Foundation
 import CoreData
 
 
-/*
- 
- STEPS TO CREATE COREDATA INTO SWIFTUI
- 1- create data model file
- 2- inside data model file create entity with some proprites on it
- 3- create empty swift file
- 4- create class inside empty swift file the class perform ObservableObject protocol
- 5- create NSPersistentContainer constant that hold the data model file name
- 6-
- 
- 
- 
- 
- */
-
 class FavoriteDataPersistanceController: ObservableObject {
     let controller = NSPersistentContainer(name: "Favorite")
     
